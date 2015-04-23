a1da560d62e44f06836284c1e43b9bf8066ba068
Martin Honermeyer <martin.honermeyer@zweitag.de>
2015-04-23 18:09:50 +0200

    Add MYNETWORKS setting
    This is needed when the SMTP port is exposed on the host.

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-postconf myhostname=$MYHOSTNAME mynetworks=172.17.42.1
+postconf myhostname=$MYHOSTNAME mynetworks="172.17.42.1 $MYNETWORKS"
 
 /usr/local/bin/syslog-stdout.py &
 exec /usr/lib/postfix/master -d
